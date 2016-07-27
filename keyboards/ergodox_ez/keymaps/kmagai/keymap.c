cd86931c159c11c63f98387cbb18459741210439
kmagai <koki.kumagai@dena.com>
2016-07-27 15:13:14 +0900

    fix

--- keyboards/ergodox_ez/keymaps/kmagai/keymap.c
+++ keyboards/ergodox_ez/keymaps/kmagai/keymap.c
@@ -17,15 +17,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |Ctrl    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  =   |  '"  |AltShf|  Cmd | Space|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |  =    |  '"  | Alt |  Cmd | Space|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | App  | LGui |       |AltSft|Ctrl/Esc|
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |      |RIGHT |       |  Up  |        |      |
+ *                                 |Backsp|DELETE|------|       |------|  Tab   |Enter |
+ *                                 |ace   |      | LEFT |       | Down |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -36,19 +36,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_EQL,         KC_QUOT,      LALT(KC_LSFT),  KC_LGUI,KC_SPC,
+        KC_EQL,         KC_QUOT,      KC_LALT,  KC_LGUI,KC_SPC,
                                               ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC,KC_END,
+                                                              KC_RGHT,
+                                               KC_BSPC,KC_DELT,KC_LEFT,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+             LALT(KC_LSFT),CTL_T(KC_ESC),
+             KC_UP,
+             KC_DOWN,KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
