0033f60a4d4c754a5a4135ca11c22751e7dfd5e3
kmagai <koki.kumagai@dena.com>
2016-08-31 14:03:02 +0900

    change arrow etc

--- keyboards/ergodox_ez/keymaps/kmagai/keymap.c
+++ keyboards/ergodox_ez/keymaps/kmagai/keymap.c
@@ -12,20 +12,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.          ,--------------------------------------------------.
  * | ~/`    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           | Ctrl |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           | /Esc |------+------+------+------+------+--------|
  * |Ctrl    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  | /Eisu|           |/Kana |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  =    |  '"  | Alt |  Cmd | Space|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       |AltSft|Ctrl/Esc|
+ *                                        ,-------------.       ,---------------.
+ *                                        | LEFT |RIGHT |       | LEFT | RIGHT  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |RIGHT |       |  Up  |        |      |
+ *                                 |      |      |  Up  |       |  Up  |        |      |
  *                                 |Backsp|DELETE|------|       |------|  Tab   |Enter |
- *                                 |ace   |      | LEFT |       | Down |        |      |
+ *                                 |ace   |      | Down |       | Down |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -35,18 +35,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT(SYMB,KC_GRV),KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_LANG2),
         KC_EQL,         KC_QUOT,      KC_LALT,  KC_LGUI,KC_SPC,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_RGHT,
-                                               KC_BSPC,KC_DELT,KC_LEFT,
+                                                     KC_LEFT,  KC_RGHT,
+                                                                 KC_UP,
+                                               KC_BSPC,KC_DELT,KC_DOWN,
         // right hand
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+       CTL_T(KC_ESC),     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+             MEH_T(KC_LANG1),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
                                   KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             LALT(KC_LSFT),CTL_T(KC_ESC),
+             KC_LEFT,KC_RGHT,
              KC_UP,
              KC_DOWN,KC_TAB, KC_ENT
     ),
